d67c0475e638d994a88a3294a7d1b26af5671841
muhai <muhai.li@tuya.com>
2021-09-10 21:05:16 +0800

    feat: scan

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,4 +1,5 @@
 export * from './get-import';
 export * from './noexcept';
+export * from './scan';
 export * from './util';
 export * from './walkFile';
---
+++ src/utils/scan.ts
@@ -0,0 +1,12 @@
+export function scan(reg: RegExp, content: string): RegExpExecArray[]
+export function scan(reg: string, content: string): RegExpExecArray[]
+export function scan(reg: RegExp | string, content: string): RegExpExecArray[] {
+  const scanner = new RegExp(reg)
+  let token = scanner.exec(content);
+  const result: RegExpExecArray[] = [];
+  while (token) {
+    result.push(token);
+    token = scanner.exec(content);
+  }
+  return result;
+};
