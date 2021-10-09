f758d52d203fbb3fb3b176a90d2ac0d1c326c49c
saber2pr <saber2pr@gmail.com>
2021-10-09 15:39:36 +0800

    chore: update

--- src/traverser/traverser.ts
+++ src/traverser/traverser.ts
@@ -22,3 +22,16 @@ export const traverse = (node: ts.Node, callback: (node: ts.Node) => void) => {
   callback(node)
   node.forEachChild(node => traverse(node, callback))
 }
+
+/**
+ * 从一段代码创建一个ast并遍历节点
+ * @deprecated use traverse instead
+ */
+export const traverseFromString = (
+  code: string,
+  callback: (node: ts.Node) => void,
+  scriptKind?: ts.ScriptKind
+) => {
+  const root = createAstNode(code, scriptKind)
+  traverse(root, callback)
+}
