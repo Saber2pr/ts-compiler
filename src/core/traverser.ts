d8a7558e9ccf019f687d3dd8755d63c20571c8d9
saber2pr <saber2pr@gmail.com>
2021-10-09 10:40:26 +0800

    fix: ScriptKind

--- src/core/traverser.ts
+++ src/core/traverser.ts
@@ -1,10 +1,19 @@
-import ts from 'typescript/lib/typescript';
+import ts from 'typescript/lib/typescript'
 
 /**
  * 创建一个ast节点
  */
-const createAstNode = (code: string) =>
-  ts.createSourceFile(`${setTimeout(() => { })}`, code, ts.ScriptTarget.ES2015, /*setParentNodes */ true);
+const createAstNode = (
+  code: string,
+  scriptKind: ts.ScriptKind = ts.ScriptKind.TSX
+) =>
+  ts.createSourceFile(
+    `${setTimeout(() => {})}`,
+    code,
+    ts.ScriptTarget.ES2015,
+    /*setParentNodes */ true,
+    scriptKind
+  )
 
 /**
  * 递归遍历ast所有节点
@@ -17,7 +26,11 @@ const traverse = (node: ts.Node, callback: (node: ts.Node) => void) => {
 /**
  * 从一段代码创建一个ast并遍历节点
  */
-export const traverseFromString = (code: string, callback: (node: ts.Node) => void) => {
-  const root = createAstNode(code)
+export const traverseFromString = (
+  code: string,
+  callback: (node: ts.Node) => void,
+  scriptKind?: ts.ScriptKind
+) => {
+  const root = createAstNode(code, scriptKind)
   traverse(root, callback)
 }
